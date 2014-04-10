0fce8c363009d0d1e030e3bbfb4ea97cf184ff82
Tero Keinänen <kenkku@kenkku.net>
2014-04-10 23:36:39 +0300

    basic one-level command parsing functionality

--- helpers/commandparser.py
+++ helpers/commandparser.py
@@ -1,4 +1,27 @@
+import collections
+
+_ParseResult = collections.namedtuple('ParseResult', ['function', 'arguments'])
+
 class CommandParser:
+    """Stores information on commands and parses messages"""
     
     def __init__(self):
-        pass
+        self.commands = {}
+
+    def add(self, command, action):
+        """Add command to parser."""
+        self.commands[command] = action
+
+    def parse(self, msg):
+        """Take message and return applicable command.
+
+        Returns:
+           Named tuple with fields function and arguments if successful,
+           otherwise None.
+        """
+        partition = msg.partition(" ")
+        command = partition[0]
+        rest = partition[2].lstrip()
+        if command in self.commands:
+            return _ParseResult(self.commands[command], rest)
+        return None
--- test/test_commandparser.py
+++ test/test_commandparser.py
@@ -6,6 +6,30 @@ class TestCommandParser(unittest.TestCase):
     def test_creating_parser(self):
         parser = CommandParser()
 
+    def test_no_matches(self):
+        parser = CommandParser()
+        self.assertEqual(parser.parse("command"), None)
+
+    def test_adding_level_1_command(self):
+        parser = CommandParser()
+        parser.add("command", None)
+
+    def test_level_1_match(self):
+        parser = CommandParser()
+        parser.add("!command", int)
+
+        self.assertEqual(parser.parse("!command").function, int)
+        example = parser.parse("!command xxx")
+        self.assertEqual(example.function, int)
+        self.assertEqual(example.arguments, "xxx")
+
+        example = parser.parse("!command  xxx")
+        self.assertEqual(example.function, int)
+        self.assertEqual(example.arguments, "xxx")
+
+        self.assertEqual(parser.parse("x!command"), None)
+
+
 
 if __name__ == '__main__':
     unittest.main()
