67035cdfa1a0abbf43c794ca51f5d5f7b1505bce
Jere Toivonen <jere.toivonen@helsinki.fi>
2014-04-11 21:26:47 +0300

    Added pirkkaniksiFeature

---
+++ Features/pirkkaniksiFeature.py
@@ -0,0 +1,18 @@
+import urllib2
+
+class pirkkaniksiFeature:
+
+    def __init__(self):
+        self.cmdpairs = {
+                "!varjoniksi" : self.execute
+                }
+
+    def execute(self, queue, nick, msg, channel):
+        try:
+            niksi = urllib2.urlopen("http://thermopylas.fi/ws/nicksit.php").read()
+        except urllib2.URLError, e:
+            print "niksi failed: " + e.reason
+            queue.put(("niksi failed: " + e.reason, channel))
+            return
+        print "executed niksi"
+        queue.put((niksi, channel))
