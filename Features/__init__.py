cc4acd49ffd543f95e57a63716b5113eb142f1d4
Tero Keinänen <kenkku@kenkku.net>
2014-04-11 19:45:08 +0300

    only load feature classes ending in Feature

--- Features/__init__.py
+++ Features/__init__.py
@@ -7,8 +7,6 @@ for loader, name, is_pkg in pkgutil.walk_packages(__path__):
     module = loader.find_module(name).load_module(name)
 
     for name, value in inspect.getmembers(module):
-        if name.startswith('__'):
-            continue
-
-        globals()[name] = value
-        __all__.append(name)
+        if name.endswith('Feature'):
+            globals()[name] = value
+            __all__.append(name)
