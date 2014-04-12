5cda69605c7f1a46df81228758a80dfb1836b9e3
Lauri Kangassalo <lauri.kangassalo@helsinki.fi>
2014-04-12 12:31:54 +0300

    fixed many bugs in horos, now uses datetime

--- Features/horosFeature.py
+++ Features/horosFeature.py
@@ -1,18 +1,21 @@
+# vim: set fileencoding=UTF-8 :
 import urllib2
+import datetime
 
 class horosFeature:
-    horokset = {"oinas": "aries", "härkä": "taurus", "harka": "taurus", "kaksoset": "gemini", "rapu": "cancer", "leijona": "leo", "neitsyt": "virgo", "vaaka": "libra", "skorpioni": "scorpion", "jousimies": "sagittarius", "kauris": "capricorn", "vesimies": "aquarius", "kalat": "pisces" }
 
     def __init__(self):
         self.cmdpairs = {
                 "!varjohoros" : self.execute
                 }
+        self.horokset = {"oinas": "aries", "härkä".decode('utf-8'): "taurus", "harka": "taurus", "kaksoset": "gemini", "rapu": "cancer", "leijona": "leo", "neitsyt": "virgo", "vaaka": "libra", "skorpioni": "scorpion", "jousimies": "sagittarius", "kauris": "capricorn", "vesimies": "aquarius", "kalat": "pisces" }
 
 
     def execute(self, queue, nick, msg, channel):
-        horosName = horokset[msg]
-        if !horosName:
-            queue.put("why won't you give me a sign~", channel)
+        msg = msg.split()[1].rstrip()
+        horosName = self.horokset.get(msg,0)
+        if not horosName:
+            queue.put(("why won't you give me a sign~", channel))
             return
         try:
             horos = urllib2.urlopen("http://www.astro.fi/future/weeklyForecast/sign/" + horosName).read()
@@ -22,40 +25,44 @@ class horosFeature:
             return
         print "fetched horos"
 
-        horos = dropHtmlPrecedingHoroscope(horos,msg)
-        horos = dropHtmlTailingHoroscope(result)
-        horos = html2Txt(horos)
+        print "msg: " + msg
+        horos = horos.decode('utf-8')
+        horos = self.dropHtmlPrecedingHoroscope(horos,msg)
+        horos = self.dropHtmlTailingHoroscope(horos)
+        horos = self.html2Txt(horos)
 
         print horos
         queue.put((horos, channel))
 
 
-    def dropHtmlPrecedingHoroscope(html, sign):
+    def dropHtmlPrecedingHoroscope(self, html, sign):
         sign = sign[1:]
-        tagCount = 2
+        print "sign: " + sign
+        tagCount = datetime.datetime.today().weekday() + 1
 
-        if tagCount > 14 || tagCount < 2:
+        if tagCount > 14 or tagCount < 2:
             return "invalid date"
 
         htmlArray = html.split(sign + "</h3><p>", tagCount + 1)
         result = htmlArray[tagCount]
 
-        if !result || len(result) < 20:
+        if (not result) or len(result) < 20:
             return "failed to parse HTML (horoscope too short)"
 
         return result
 
-    def dropHtmlTailingHoroscope(html):
+    def dropHtmlTailingHoroscope(self, html):
         html = html.split("<br />", 1)
-        result = html[0]
+        result = html[0][:-4]
 
-        if !result || len(result) > 500:
+        if not result or len(result) > 500:
             return "failed to parse HTML (horoscope too long)"
 
         return result
 
 
-    def html2Txt(html):
+    def html2Txt(self, html):
+        html = html.encode('utf-8')
         html = html.replace("&auml;", "ä")
         html = html.replace("&ouml;", "ö")
         html = html.replace("&Auml;", "Ä")
