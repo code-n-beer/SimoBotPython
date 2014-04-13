c2a33bb20696399182d787bc3e12cbac1516952c
Tero Keinänen <kenkku@kenkku.net>
2014-04-14 02:36:53 +0300

    only connect when start() called, autojoin channels, handle nick in use

--- SimoBot.py
+++ SimoBot.py
@@ -3,15 +3,35 @@ import logging
 
 logging.basicConfig(level=logging.DEBUG)
 
+
 class SimoBot(SimpleIRCClient):
 
-    def __init__(self, server, port, nick):
+    def __init__(self, server, port, nick, channels=[]):
         super(SimoBot, self).__init__()
-        self.connect(server=server,
-                     port=port,
-                     nickname=nick)
+        self.connection.set_rate_limit(1)
+        self.server = server
+        self.port = port
+        self.nick = nick
+        self.channels = channels
+
+    def send_to_chan(self, chan, msg):
+        self.connection.privmsg(chan, msg)
+
+    def on_nicknameinuse(self, c, e):
+        self.nick = c.nickname + "_"
+        c.nick(self.nick)
+
+    def on_welcome(self, c, e):
+        for channel in self.channels:
+            c.join(channel)
 
     def on_pubmsg(self, c, e):
         msg = e.arguments[0]
         logging.debug("message on " + e.target +
                       ": <" + e.source.nick + "> " + msg)
+
+    def start(self):
+        self.connect(server=self.server,
+                     port=self.port,
+                     nickname=self.nick)
+        super(SimoBot, self).start()
