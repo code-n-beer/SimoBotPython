e59384c31cead36edd70ff8d3263700d54b80949
Lauri Kangassalo <lauri.kangassalo@helsinki.fi>
2014-04-16 00:04:09 +0300

    Added unicafeFeature

---
+++ Features/unicafeFeature.py
@@ -0,0 +1,71 @@
+# vim: set fileencoding=UTF-8 :
+import urllib2
+import xml.etree.ElementTree as ET
+import datetime
+import re
+
+class unicafeFeature:
+
+    def __init__(self):
+        self.cmdpairs = {
+                "!varjouc" : self.execute
+                }
+
+    def execute(self, queue, nick, msg, channel):
+      menuExa = ""
+      menuChem = ""
+      try:
+        menuExa = urllib2.urlopen("http://www.hyyravintolat.fi/rss/fin/11/").read()
+        menuChem = urllib2.urlopen("http://www.hyyravintolat.fi/rss/fin/10/").read()
+      except urllib2.URLError, e:
+          print "fetching menu failed: " + e.reason
+          queue.put(("fetching menu failed: " + e.reason, channel))
+          return
+        
+      today = datetime.datetime.today().weekday() 
+
+      exaLunch = self.parseRSSString(menuExa, "Exactum", today)
+      chemLunch = self.parseRSSString(menuChem, "Chemicum", today)
+      result = exaLunch + " /::/ " + chemLunch
+
+      print result
+      queue.put((result, channel))
+
+    def parseRSSString(self, menuRSS, restaurant, today):
+      root = ET.fromstring(menuRSS)
+      root = root.findall("./channel/item/description")
+
+      result = "no lunch today"
+      if today < len(root):
+        tmpstring = "<root>" + root[today].text.encode('utf-8').replace(">,",">") + "</root>"
+        todaysMenu = ET.XML(tmpstring)
+        todaysPrices = todaysMenu.findall(".p/span[@class='priceinfo']")
+        todaysMenu = todaysMenu.findall("./p/span[@class='meal']")
+        
+        # tän vois tehdä nätimmin
+        todaysMenuStrings = []
+        i = 0
+
+        for menu in todaysMenu:
+          # vittuun ne päivän hedelmät
+          menuString = re.sub(r' \( .* \)$','',menu.text)
+
+          if i < len(todaysPrices) and "Maukkaasti" in todaysPrices[i].text:
+            menuString += " [M]"
+
+          todaysMenuStrings.append(menuString)
+          i = i + 1
+            
+        result = " // ".join(todaysMenuStrings)
+
+      result = restaurant + ": "+result
+      return result
+
+
+    def html2Txt(self, html):
+        html = html.encode('utf-8')
+        html = html.replace("&auml;", "ä")
+        html = html.replace("&ouml;", "ö")
+        html = html.replace("&Auml;", "Ä")
+        html = html.replace("&Ouml;", "Ö")
+        return html
