194ca8aede94f8b6d1d66c6ee0dc41325f81e2c4
Kalle Viiri <kviiri@cs.helsinki.fi>
2014-04-25 19:28:14 +0300

    finished explFeature

--- Features/explFeature.py
+++ Features/explFeature.py
@@ -36,12 +36,12 @@ class explFeature:
           queue.put(("Invalid expl index", channel))
           return
     explrange = self.redis.lrange(topic, 0, self.redis.llen(topic))
-    ret = topic + "[" + str(explIndex) + "/{}] : "
+    ret = (topic + "{} : ").encode('utf-8')
     index = 0
     length = len(ret)
     page = 1
     while index < len(explrange):
-      addString = str(index + 1) + ") " + explrange[index] + "  "
+      addString = "|" + str(index + 1) + "| " + explrange[index].rstrip()  + "  "
       nextLength = len(addString)
       #ignore too long expls (in case any get through anyway)
       if nextLength > self.msglength:
@@ -57,7 +57,12 @@ class explFeature:
     if explIndex > page:
       queue.put(("Invalid expl index", channel))
       return
-    queue.put((ret.format(page), channel))  
+    if page == 1:
+      ret = ret.format("")
+    else:
+      ret = ret.format("[" + str(explIndex) + "/" + str(page) + "]")
+    queue.put((ret, channel))  
+
 
 
   def add(self, queue, nick, msg, channel):
@@ -71,9 +76,37 @@ class explFeature:
     if len(msg[2]) > self.msglength:
       queue.put(("Expl too long, not added", channel))
       return
-    self.redis.rpush(msg[1], msg[2])
+    self.redis.rpush(msg[1], msg[2].rstrip())
     queue.put(("New expl added!", channel))
 
   def remove(self, queue, nick, msg, channel):
-    queue.put(("Not implemented yet", channel))
+    msg = msg.split()
+    if len(msg) == 2:
+      if not self.redis.exists(msg[1]):
+        queue.put(("No such expl", channel))
+        return
+      self.redis.delete(msg[1])
+      queue.put(("Removed one expl topic!", channel))
+      return
+    elif len(msg) != 3:
+      queue.put(("usage: !remove <topic> [index]", channel))
+      return
+    elif not self.redis.exists(msg[1]):
+      queue.put(("no such expl", channel))
+      return
+    elif int(msg[2]) <= 0 or int(msg[2]) > self.redis.llen(msg[1]):
+      queue.put(("bad index", channel))
+      return
+    if int(msg[2]) == 1:
+      newRangeHead = {}
+    else:
+      newRangeHead = self.redis.lrange(msg[1], 0, int(msg[2]) - 2)
+    print(newRangeHead)
+    newRangeTail = self.redis.lrange(msg[1], int(msg[2]), self.redis.llen(msg[1]))
+    self.redis.delete(msg[1])
+    for i in newRangeHead:
+      self.redis.rpush(msg[1], i)
+    for i in newRangeTail:
+      self.redis.rpush(msg[1], i)
+    queue.put(("Deleted one expl item!", channel))
 
