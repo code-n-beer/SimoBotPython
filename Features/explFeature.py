5b139a0f25e4b38ebe9892fea30bd893a51afd99
Kalle Viiri <kviiri@cs.helsinki.fi>
2014-04-27 20:01:06 +0300

    a neater delimiter

--- Features/explFeature.py
+++ Features/explFeature.py
@@ -46,7 +46,7 @@ class explFeature:
     length = len(ret)
     page = 1
     while index < len(explrange):
-      addString = "|" + str(index + 1) + "| " + explrange[index].rstrip()  + "  "
+      addString = "[" + str(index + 1) + "] " + explrange[index].rstrip()  + "  "
       nextLength = len(addString)
       #ignore too long expls (in case any get through anyway)
       if nextLength > self.msglength:
