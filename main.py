cb43e717b28247640d53595dffa8287c16c40631
Lauri Kangassalo <lauri.kangassalo@helsinki.fi>
2014-04-28 20:00:24 +0300

    Now supports URLs in the middle of message, also added XHTML support

--- main.py
+++ main.py
@@ -144,7 +144,7 @@ while 1:
         #reload(Features)
         #irc.send("PRIVMSG " + channel + " :sis t. varjosimo\r\n");
 
-    if msg.startswith("http") or msg.startswith("www"):
+    if "http" in msg or "www" in msg:
         print "dispatching urltitle"
         p = Process(target=commands["http"], args=(q, nick, msg, channel))
         p.start()
