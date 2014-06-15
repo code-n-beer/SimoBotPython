f0e7880c243f14c5e6c30675cb385932a779c2bb
iorena <essi.salmenkivi@helsinki.fi>
2014-06-15 18:45:02 +0300

    final final fix

--- Features/expltriviaFeature.py
+++ Features/expltriviaFeature.py
@@ -79,7 +79,7 @@ class explTriviaFeature:
 			else:
 				simomsg = "Wrong. What expl? " + str(self.redisAnswer.get("question")) + " | " + str(self.redisAnswer.get("hint1"))
 				if qpoints <5:
-					simomsg = simomsg + " | " + self.redisAnswer.get("hint2")
+					simomsg = simomsg + " | " + str(self.redisAnswer.get("hint2"))
 				simomsg = simomsg + " (" + str(int(qpoints)-2) + " points)"
 				self.redisAnswer.set("points",int(qpoints)-2)
 		queue.put((simomsg, channel))
