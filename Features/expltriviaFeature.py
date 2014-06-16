1da6321d82e29f8a4d3c0269d2c90c36f257a015
iorena <essi.salmenkivi@helsinki.fi>
2014-06-16 17:10:25 +0300

    trivia hints completed

--- Features/expltriviaFeature.py
+++ Features/expltriviaFeature.py
@@ -56,7 +56,7 @@ class explTriviaFeature:
 
 
 	def answer(self, queue, nick, msg, channel):
-		if not self.redisAnswer.exists("answer"):
+		if self.redisAnswer.exists("answer") != 1:
 			return
 		answer = str(self.redisAnswer.get("answer"))
 		simomsg = ""
