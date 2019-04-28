d368af33b4d0f073091aebbfc3add97a0fd5d945
Teemu Sarapisto <tsarpf@gmail.com>
2019-04-28 22:43:39 +0300

    Added x minus y plus z

--- Features/word2vecFeature.py
+++ Features/word2vecFeature.py
@@ -6,14 +6,59 @@ class word2vecFeature:
        self.cmdpairs = {
            "!similar": self.execute,
            "!similarn": self.executen,
-           "!likexytoz": self.executexyz
+           "!likexytoz": self.executexyz,
+            "!xminusyplusz": self.execute_x_minus_y_plus_z
        }
        self.model = gensim.models.Word2Vec.load("./Resources/word2vec_2014-2019_04.model")
+
+    def x_plus_y(self, x, y):
+        return self.model.wv.similar_by_vector(
+            self.model.wv.get_vector(x)
+            + self.model.wv.get_vector(y))
+    
+    def x_minus_y(self, x, y):
+        return self.model.wv.similar_by_vector(
+            self.model.wv.get_vector(x)
+            - self.model.wv.get_vector(y))
+        
+    def x_plus_y_minus_z(self, x, y, z):
+        return self.x_minus_y(self.x_plus_y(x, y), z)
+
+    def x_plus_y_plus_z(self, x, y, z):
+        return self.x_plus_y(self.x_plus_y(x, y), z)
+
+    def x_minus_y_plus_z(self, x, y, z):
+        return self.x_plus_y(self.x_minus_y(x, y), z)
+
+    def x_minus_y_minus_z(self, x, y, z):
+        return self.x_minus_y(self.x_minus_y(x, y), z)
+    
+
+    def execute_x_minus_y_plus_z(self, queue, nick, msg, channel):
+        words = msg.split()
+        if len(words) < 4:
+            print("nothing to similarize")
+            return
+
+        x = words[1]
+        y = words[2]
+        z = words[3]
+
+        result = ""
+        try:
+            m = self.x_minus_y_plus_z(x,y,z)
+            result = m
+        except(KeyError):
+            result = 'not found'
+
+        ret_val = result.encode('utf-8').strip()
+        queue.put((ret_val, channel))
+
 
     def executexyz(self, queue, nick, msg, channel):
         words = msg.split()
         if len(words) < 4:
-            print("nothing to similarize")
+            queue.put(('nothing to similarize', channel))
             return
 
         x = words[1]
