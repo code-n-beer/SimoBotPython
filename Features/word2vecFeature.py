efef905378b692fc6501a67a25f81d17b7d146d3
tsarpf <tsarpf@gmail.com>
2019-04-28 03:43:48 +0300

    Add xy to z feature to word2vec stuff

--- Features/word2vecFeature.py
+++ Features/word2vecFeature.py
@@ -4,10 +4,29 @@ class word2vecFeature:
 
     def __init__(self):
        self.cmdpairs = {
-           "!similar": self.execute
+           "!similar": self.execute,
+           "!likexytoz": self.executexyz
        }
        self.model = gensim.models.Word2Vec.load("./Resources/word2vec_2016-2019_04.model")
 
+    def executexyz(self, queue, nick, msg, channel):
+        words = msg.split()
+        if len(words) < 4:
+            print("nothing to similarize")
+            return
+
+        x = words[1]
+        y = words[2]
+        z = words[3]
+
+        m = self.model.wv.most_similar (positive=[z, x], negative=[y])
+        results = []
+        for result in m:
+            val, prob = result
+            results.append(val)
+
+        ret_val = ' '.join(results)
+        queue.put((ret_val, channel))
 
     def execute(self, queue, nick, msg, channel):
         words = msg.split()
