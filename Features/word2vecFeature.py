6fc1b2889089ecf9b3229350fe34aef0ecaf59d0
Teemu Sarapisto <tsarpf@gmail.com>
2019-04-29 19:39:59 +0300

    Add yle vec features

--- Features/word2vecFeature.py
+++ Features/word2vecFeature.py
@@ -1,25 +1,32 @@
 import gensim 
+from gensim.models import KeyedVectors
 
 class word2vecFeature:
 
     def __init__(self):
        self.cmdpairs = {
-           "!similar": self.execute,
-           "!similarn": self.executen,
-           "!likexytoz": self.executexyz,
-            "!xminusyplusz": self.execute_x_minus_y_plus_z
-       }
-       self.model = gensim.models.Word2Vec.load("./Resources/word2vec_2014-2019_04.model")
+           "!similar": self.execute_cnb,
+           "!similaryle": self.execute_yle,
 
-    def x_plus_y(self, x, y):
-        return self.model.wv.get_vector(x) + self.model.wv.get_vector(y)
+           "!similarn": self.execute_n_cnb,
+           "!similarnyle": self.execute_n_yle,
+
+           "!xminusyplusz": self.execute_xyz_cnb,
+           "!xminusypluszyle": self.execute_xyz_yle,
+#            "!xminusyplusz": self.execute_x_minus_y_plus_z
+       }
+       self.cnb_wv = gensim.models.Word2Vec.load("./Resources/word2vec_2014-2019_04.model").wv
+       self.yle_wv = KeyedVectors.load("./Resources/word2vec_yle_dersb")
+
+    #def x_plus_y(self, x, y):
+    #    return self.model.wv.get_vector(x) + self.model.wv.get_vector(y)
     
-    def x_minus_y(self, x, y):
-        return self.model.wv.get_vector(x) - self.model.wv.get_vector(y)
-        
-    def x_minus_y_plus_z(self, x, y, z):
-        return self.model.wv.similar_by_vector(
-            self.x_plus_y(self.x_minus_y(x, y), z))
+    #def x_minus_y(self, x, y):
+    #    return self.model.wv.get_vector(x) - self.model.wv.get_vector(y)
+    #    
+    #def x_minus_y_plus_z(self, x, y, z):
+    #    return self.model.wv.similar_by_vector(
+    #        self.x_plus_y(self.x_minus_y(x, y), z))
 
     #def x_plus_y_minus_z(self, x, y, z):
     #    return self.model.wv.similar_by_vector(
@@ -28,32 +35,35 @@ class word2vecFeature:
     #    return self.x_plus_y(self.x_plus_y(x, y), z)
     #def x_minus_y_minus_z(self, x, y, z):
     #    return self.x_minus_y(self.x_minus_y(x, y), z)
-    
+    #def execute_x_minus_y_plus_z(self, queue, nick, msg, channel):
+    #    words = msg.split()
+    #    if len(words) < 4:
+    #        print("nothing to similarize")
+    #        return
 
-    def execute_x_minus_y_plus_z(self, queue, nick, msg, channel):
-        words = msg.split()
-        if len(words) < 4:
-            print("nothing to similarize")
-            return
+    #    x = words[1]
+    #    y = words[2]
+    #    z = words[3]
 
-        x = words[1]
-        y = words[2]
-        z = words[3]
+    #    results = []
+    #    try:
+    #        m = self.x_minus_y_plus_z(x,y,z)
+    #        for word, _ in m:
+    #            if word != x and word != y and word != z:
+    #                results.append(word)
+    #    except(KeyError):
+    #        results.append('not found')
 
-        results = []
-        try:
-            m = self.x_minus_y_plus_z(x,y,z)
-            for word, _ in m:
-                if word != x and word != y and word != z:
-                    results.append(word)
-        except(KeyError):
-            results.append('not found')
+    #    ret_val = u' '.join(results).encode('utf-8').strip()
+    #    queue.put((ret_val, channel))
 
-        ret_val = u' '.join(results).encode('utf-8').strip()
-        queue.put((ret_val, channel))
+    def execute_xyz_cnb(self, queue, nick, msg, channel):
+        self.executexyz(queue, nick, msg, channel, self.cnb_wv)
 
+    def execute_xyz_yle(self, queue, nick, msg, channel):
+        self.executexyz(queue, nick, msg, channel, self.yle_wv)
 
-    def executexyz(self, queue, nick, msg, channel):
+    def executexyz(self, queue, nick, msg, channel, wv):
         words = msg.split()
         if len(words) < 4:
             queue.put(('nothing to similarize', channel))
@@ -64,20 +74,30 @@ class word2vecFeature:
         z = words[3]
 
         try:
-            m = self.model.wv.most_similar (positive=[z, x], negative=[y])
+            m = wv.most_similar (positive=[z, x], negative=[y])
         except(KeyError):
             queue.put(('Not found', channel))
 
         results = []
         for result in m:
-            val, prob = result
+            val, _ = result
             results.append(val)
 
         ret_val = u' '.join(results).encode('utf-8').strip()
 
         queue.put((ret_val, channel))
+    
+    def execute_cnb(self, queue, nick, msg, channel):
+        self.execute(queue, nick, msg, channel, self.cnb_wv)
 
-    def execute(self, queue, nick, msg, channel):
+    def execute_yle(self, queue, nick, msg, channel):
+        self.execute(queue, nick, msg, channel, self.yle_wv)
+
+    def get_m(self, wv, word):
+        word = word.lower()
+        return wv.most_similar (positive=word)
+
+    def execute(self, queue, nick, msg, channel, wv):
         words = msg.split()
         if len(words) < 2:
             print("nothing to similarize")
@@ -87,18 +107,24 @@ class word2vecFeature:
 
         word = words[1]
         try:
-            m = self.model.wv.most_similar (positive=word.lower())
+            m = self.get_m(wv, word)
         except(KeyError):
             queue.put((word + ' not found', channel))
         results = []
         for result in m:
-            val, prob = result
+            val, _ = result
             results.append(val)
 
         ret_val = u' '.join(results).encode('utf-8').strip()
         queue.put((ret_val, channel))
 
-    def executen(self, queue, nick, msg, channel):
+    def execute_n_cnb(self, queue, nick, msg, channel):
+        self.execute_n(queue, nick, msg, channel, self.cnb_wv)
+
+    def execute_n_yle(self, queue, nick, msg, channel):
+        self.execute_n(queue, nick, msg, channel, self.yle_wv)
+
+    def execute_n(self, queue, nick, msg, channel, wv):
         words = msg.split()
         if len(words) < 2:
             print("nothing to similarize")
@@ -108,7 +134,7 @@ class word2vecFeature:
         results = []
         for word in words:
             try:
-                m = self.model.wv.most_similar (positive=word.lower())[0][0]
+                m = wv.most_similar (positive=word.lower())[0][0]
                 results.append(m)
             except(KeyError):
                 results.append(word)
